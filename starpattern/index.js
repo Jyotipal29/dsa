98ed616d7af436304c588baa0be0ccb2e197e533
jyoti pal <paljyoti0129@gmail.com>
2024-05-14 23:37:41 +0530

    solved star patterns

---
+++ starpattern/index.js
@@ -0,0 +1,78 @@
+//1. print the start pattern
+
+// * * * *
+// * * * *
+// * * * *
+
+
+
+// const printStar = () => {
+//     for (let i = 0; i < 4; i++) {
+//         let row = ""
+//         for (let j = 0; j < 4; j++) {
+//             row += "* "
+//         }
+//         console.log(row)
+//     }
+// }
+
+// printStar()
+
+
+
+
+// 2
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+// * * * * * *
+
+
+// const print2 = (n) => {
+//     for (let i = 0; i < n; i++) {
+//         let row = " "
+//         for (let j = 0; j <= i; j++) {
+//             row += "* "
+//         }
+
+//         console.log(row)
+//     }
+// }
+// print2(5)
+
+
+// 3
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+// 1 2 3 4 5 6
+
+
+// const print3 = (n) => {
+//     for (let i = 1; i <= n; i++) {
+//         let row = ""
+//         for (let j = 1; j <= i; j++) {
+//             row += j + " "
+
+//         }
+//         console.log(row)
+
+
+//     }
+// }
+
+// print3(5)
+
+
+
+
+//4
+// 1
+// 2 2
+// 3 3 3
+// 4 4 4 4
+// 5 5 5 5 5
