4f1cda6da2da26c8dd01d7ba491568281f0b8813
jyoti pal <paljyoti0129@gmail.com>
2024-05-15 23:58:26 +0530

    solved patterns

--- starpattern/index.js
+++ starpattern/index.js
@@ -71,8 +71,86 @@
 
 
 //4
+// if row 1 then 1 time 1
+// if row 2 then 2 time 2
 // 1
 // 2 2
 // 3 3 3
 // 4 4 4 4
 // 5 5 5 5 5
+
+// const print = (n) => {
+//     for (let i = 1; i < n; i++) {
+//         let row = '';
+//         for (let j = 1; j <= i; j++) {
+//             row += i + " "
+//         }
+
+//         console.log(row)
+//     }
+// }
+
+// print(5)
+
+
+//5
+// * * *
+// * *
+// *
+
+
+// const print = (n) => {
+//     for (let i = n; i >= 1; i--) {
+//         let row = "";
+//         for (let j = 0; j <= i; j++) {
+//             row += "*  "
+//         }
+
+//         console.log(row)
+//     }
+// }
+// print(5)
+
+// 6
+// 1 2 3
+// 1 2
+// 1
+
+// const print = (n) => {
+//     for (let i = n; i >= 1; i--) {
+//         let row = " ";
+
+//         for (let j = 1; j <= i; j++) {
+//             row += j + " "
+//         }
+
+//         console.log(row)
+//     }
+// }
+// print(5)
+
+
+// 7
+//   *
+//  ***
+// *****
+
+
+const print = (n) => {
+    for (let i = 0; i <= n; i++) {
+        let row = " ";
+        for (let j = 0; j <= i; j++) {
+            row += "*"
+        }
+
+        console.log(row)
+    }
+}
+
+print(5)
+
+
+
+
+
+
